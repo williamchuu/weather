7bb3b70387d3d5ea945acc9426caac9bd6c649c6
William <wchu41@my.bcit.ca>
2023-02-24 20:47:52 -0800

    test 9

--- pages/index.js
+++ pages/index.js
@@ -108,9 +108,9 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if (grabWeather.current === true) {
-      fetchWeather();
-    }
+    // if (grabWeather.current === true) {
+    fetchWeather();
+    // }
 
     return () => {
       grabWeather.current = true;
